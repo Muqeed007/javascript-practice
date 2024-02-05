312aa1dc4f11d44d2e73dd19cb9e3d85dc9cf0e5
Muqeed007 <ahmedalmuqeed2002@gmail.com>
2024-02-05 13:54:05 +0600

    added sumOfEvens

--- sum-of-numbers.js
+++ sum-of-numbers.js
@@ -1,16 +1,30 @@
- function sumOfNumbers(numbers){
-     let sum = 0;
-     console.log(number);
-     for(const number of numbers){
-         console.log(number);
-         sum = sum + number;
-     }
-     return sum;
- }
+//  function sumOfNumbers(numbers){
+//      let sum = 0;
+//      console.log(number);
+//      for(const number of numbers){
+//          console.log(number);
+//          sum = sum + number;
+//      }
+//      return sum;
+//  }
 
- // const number = [1,2,3,4];
- // const sum = sumOfNumbers(number);
- // console.log('sum of numbers:',sum);
- const number = [10,20,30,40,50,60];
- const sum = sumOfNumbers(number);
- console.log('Sum of numbers =',sum);
+//  // const number = [1,2,3,4];
+//  // const sum = sumOfNumbers(number);
+//  // console.log('sum of numbers:',sum);
+//  const number = [10,20,30,40,50,60];
+//  const sum = sumOfNumbers(number);
+//  console.log('Sum of numbers =',sum);
+
+function sumOfEvens(numbers) {
+    let sum = 0;
+    for (const number of numbers) {
+        if (number % 2 === 0) {
+            console.log(number);
+            sum = sum + number;
+        }
+    }
+    return sum;
+}
+const number = [1,2,3,4,5,6,7,8,9];
+const sum = sumOfEvens(number)
+console.log('Sum of Evens =', sum);
