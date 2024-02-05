e37adc552a08014022dde88b83d6977f8094c11e
Muqeed007 <ahmedalmuqeed2002@gmail.com>
2024-02-05 13:35:25 +0600

    added sum-of-numbers

---
+++ sum-of-numbers.js
@@ -0,0 +1,16 @@
+ function sumOfNumbers(numbers){
+     let sum = 0;
+     console.log(number);
+     for(const number of numbers){
+         console.log(number);
+         sum = sum + number;
+     }
+     return sum;
+ }
+
+ // const number = [1,2,3,4];
+ // const sum = sumOfNumbers(number);
+ // console.log('sum of numbers:',sum);
+ const number = [10,20,30,40,50,60];
+ const sum = sumOfNumbers(number);
+ console.log('Sum of numbers =',sum);
